b4ff5ae456fd509d75d78ca1d88a759b4757e751
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-09-21 20:17:39 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,214 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    const url = "http://localhost:3000/books/"
+
+const getBooks = () => {
+    fetch(url)
+    .then(response => response.json())
+    .then(books => {
+        renderBooksBar(books)
+        renderBooksPage(books)
+    })
+
+}
+
+
+const renderBooksBar = books => {
+    for(const book of books){
+        renderBookBar(book)
+    }
+
+}
+
+
+const renderBookBar = book => {
+    const list = document.querySelector('#list')
+    const bookTitleLi = document.createElement('li')
+    bookTitleLi.dataset.id = book.id
+    bookTitleLi.textContent = book.title 
+    list.append(bookTitleLi)
+    bookTitleLi.dataset.id = book.id
+    bookTitleLi.classList.add('links')
+}
+
+const renderBooksPage = books => {
+    for(const book of books){
+        renderBookPage(book)
+    }
+}
+
+const renderBookPage = book => {
+    const showPanel = document.querySelector('#show-panel')
+    const bookDiv = document.createElement('div')
+    bookDiv.classList.add('book-page')
+    bookDiv.style.display = "none"
+    bookDiv.dataset.id = book.id
+    bookDiv.innerHTML = `
+        <img src='${book.img_url}'>
+        <h3>${book.title}</h3>
+        <h3>${book.subtitle}</h3>
+        <h3>${book.author}</h3>
+        <p>${book.description}</p>
+        <button class="like-button">${buttonContent(book)}</button>
+
+    `
+    showPanel.append(bookDiv)
+    const button = bookDiv.querySelector('button')
+    bookDiv.insertBefore(addUsersWhoLike(book), button )
+}
+
+const addUsersWhoLike = book => {
+    const ul = document.createElement('ul')
+    ul.classList.add('book-ul')
+    ul.dataset.id = book.id
+    for(const user of book.users){
+        const likeLi = document.createElement('li')
+        likeLi.classList.add('likes')
+        likeLi.textContent = user.username
+        ul.append(likeLi)
+    }
+    return ul
+}
+
+const buttonContent = book => {
+    for(const user of book.users){
+        if(user.id === 1){
+            return "UNLIKE"
+        } 
+    }
+    return "LIKE"
+}
+
+const clickHandler = () => {
+    document.addEventListener('click', e => {
+        if(e.target.matches('.links')){
+            const bookLink = e.target
+            const bookId = bookLink.dataset.id
+            const bookPagesContainer = document.querySelector('#show-panel')
+            const booksDiv = document.querySelectorAll('.book-page')
+            for(const book of booksDiv){
+                book.style.display = 'none'
+            }
+            const matchingBookDiv = bookPagesContainer.querySelector(`[data-id ='${bookId}']`)
+            matchingBookDiv.style.display = "block"
+        } else if (e.target.matches('.like-button')){
+            if(e.target.innerText === "LIKE"){
+            const bookId = e.target.parentElement.dataset.id
+
+            getUsersFromBook(bookId)
+            .then(users => {
+                const newUser = {
+                    id: 1,
+                    username: 'pouros'
+                }
+                users.push(newUser)
+                const bookUsers = {
+                    users
+                }
+
+                const options = {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accepts": "appication/json"
+                    },
+                    body: JSON.stringify(bookUsers)
+                }
+
+                fetch(url+bookId, options)
+                .then(response => response.json())
+                .then(book => {
+                    const bookPagesContainer = document.querySelector('#show-panel')
+                    const matchingBookDiv = bookPagesContainer.querySelector(`[data-id ='${book.id}']`)
+                    const bookUl = matchingBookDiv.querySelector('ul')
+                    const likeLi = document.createElement('li')
+                    const lastUser = book.users.pop()
+                    console.log(lastUser)
+                    likeLi.textContent = lastUser.username
+                    bookUl.append(likeLi)
+                    
+                    const likeButton = matchingBookDiv.querySelector('button')
+                    console.log(likeButton)
+                    likeButton.innerText = "UNLIKE"
+                })
+            })
+            } else if(e.target.innerText === "UNLIKE"){
+                const bookId = e.target.parentElement.dataset.id
+                getUsersFromBook(bookId)
+                .then(users => {
+                    
+                    users.pop()
+                    const bookUsers = {
+                        users 
+                    }
+                    const options = {
+                        method: "PATCH",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Accepts": "appication/json"
+                        },
+                        body: JSON.stringify(bookUsers)
+                    }
+                    fetch(url+bookId, options)
+                    .then(response => response.json())
+                    .then(book => {
+                        const bookPagesContainer = document.querySelector('#show-panel')
+                    const matchingBookDiv = bookPagesContainer.querySelector(`[data-id ='${book.id}']`)
+                    const bookUl = matchingBookDiv.querySelector('ul')
+                    bookUl.querySelector('li:last-child').remove()
+
+                    
+                    const likeButton = matchingBookDiv.querySelector('button')
+                    console.log(likeButton)
+                    likeButton.innerText = "LIKE"
+
+
+                    })
+
+                })
+
+                
+            }
+            
+           
+            // fetch(url+bookId)
+            // .then(response => response.json())
+            // .then(book => {
+            //     const currentBook = book.users
+            //     return currentBook
+            // })
+        }
+
+    })
+}
+
+const getUsersFromBook = bookId => {
+    return fetch(url+bookId)
+    .then(response => response.json())
+    .then(book => book.users)
+}
+
+getBooks()
+clickHandler()
+// buttonContent({
+//     "id": 1,
+//     "title": "Grapefruit",
+//     "subtitle": "A book of Instruction and Drawings.",
+//     "description": "Back in print for the first time in nearly thirty years, here is Yoko Ono's whimsical, delightful, subversive, startling book of instructions for art and for life. 'Burn this book after you've read it.' -- Yoko 'A dream you dream alone may be a dream, but a dream two people dream together is a reality. This is the greatest book I've ever burned.' -- John",
+//     "author": "Yoko Ono",
+//     img_url: "https://books.google.com/books/content?id=3S8Rwr-iBdoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+//     users: [
+//     {
+//     id: 2,
+//     username: "auer"
+//     },
+//     {
+//     id: 1,
+//     username: "maverick"
+//     }
+//     ]
+//     })
+
+});
+
+
+//create all books then hide and re render based on click
